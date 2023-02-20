cd40b7e54691c20ed76c05009930653b4777ee0a
lsw <free_ms@naver.com>
2023-02-20 16:14:54 +0900

    ex14425

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.StringTokenizer;
+
+public class Main {
+	// Question.:  [] : 2023-02-20
+	public static void main(String[] args) throws IOException {
+		
+	}
+}
