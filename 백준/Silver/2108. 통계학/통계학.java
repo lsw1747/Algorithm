a91d08521a110746b917236acdb6efa466200d22
lsw1747 <71085146+lsw1747@users.noreply.github.com>
2023-04-07 10:46:40 +0900

    [Silver III] Title: 통계학, Time: 1108 ms, Memory: 68144 KB -BaekjoonHub

--- 백준/Silver/2108. 통계학/통계학.java
+++ 백준/Silver/2108. 통계학/통계학.java
@@ -7,31 +7,26 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int N = Integer.parseInt(br.readLine());
+        int sum = 0, num = 0, size = 0;
+
         List<Integer> list = new ArrayList<>();
 
         for (int i = 0; i < N; i++) {
-            list.add(Integer.parseInt(br.readLine()));
+            num = Integer.parseInt(br.readLine());
+            sum += num;
+            list.add(num);
         }
 
-        int middleValue = list.size() / 2;
+        size = list.size();
+
+        int middleValue = size / 2;
         Collections.sort(list);
 
-        System.out.println(getAvg(list)); //평균값 출력
+        System.out.println((int) Math.round((((double)sum / size) / 10) * 10)); //평균값 출력
         System.out.println(list.get(middleValue)); //중간값 출력
         System.out.println(getMode(list)); //최빈값 출력
-        System.out.println(list.get(list.size() - 1) - list.get(0)); //최댓값 - 최솟값 출력
+        System.out.println(list.get(size - 1) - list.get(0)); //최댓값 - 최솟값 출력
     }
-
-    private static int getAvg(List<Integer> list) {
-        double num = 0;
-        int size = list.size();
-        for (int i = 0; i < size; i++) {
-            num += list.get(i);
-        }
-        num = num / size;
-        return (int) Math.round((num / 10) * 10);
-    }
-
     private static int getMode(List<Integer> list) {
         //Map에 넣은 뒤, containsKey 해서 있을 시 ++한 값 입력
         HashMap<Integer, Integer> map = new HashMap<>();
